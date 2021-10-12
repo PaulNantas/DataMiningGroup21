aeb39a854df46474195a2f5d3f87cdf1971e0b3a
PaulNantas <paulnantas2@gmail.com>
2021-10-12 20:26:15 +0200

    Adding pre-processing, normalization, country by id ..

--- regression.py
+++ regression.py
@@ -1,13 +1,16 @@
+from pandas.io.parsers import read_csv
 from create_dataset import *
 
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 import matplotlib
 import matplotlib.pyplot as plt
-
+#import seaborn as sns
+import statsmodels.api as sm
 
 #Function that return DataFrame with id corresponding to a country and 
 #a list countries_id to know which id correspond to which country
@@ -30,6 +33,12 @@ def country_to_id(df):
 
   return(df,countries_id)
 
+def continent_to_id(df):
+  continent = df['ParentLocation']
+  continent_id = {'Eastern Mediterranean':0, 'Europe':1, 'Africa':2 ,'Americas':3, 'Western Pacific':4,'South-East Asia':5 }
+  id = continent.map(continent_id)
+  df["continent_id"] = id
+  return(df,continent_id)
 #Function returns datadframe with the sex selected and the year. if flag_year = False, we don't care about years
 def choose_year_sex(df,sex_choice,flag_year,year=2014): #sex_choice = 'MLE','FMLE','BTSX'
   df_new = df.loc[df['Dim1ValueCode'] == sex_choice]
@@ -43,80 +52,138 @@ def choose_year_sex(df,sex_choice,flag_year,year=2014): #sex_choice = 'MLE','FML
 
   return(df_new)
 
-#Return a DataFrame with squarred data added
-def squared_data(df):
-    df['Squared AR'] = np.square(df['AR'])
-    df['Squared MBMI'] = np.square(df['MBMI'])
-    df['Squared FPM'] = np.square(df['FPM'])
-    df['Squared ABR'] = np.square(df['ABR'])
-
+#Return a dataframe with for each data those column
+def order_data(df,order):
+    for k in range(2,order+1):
+      features = ['AR','MBMI','FPM','ABR']
+      df[f"Power {k} of {features[0]}"] = np.power(df[features[0]],k)
+      df[f"Power {k} of {features[1]}"] = np.power(df[features[1]],k)
+      df[f"Power {k} of {features[2]}"] = np.power(df[features[2]],k)
+      df[f"Power {k} of {features[3]}"] = np.power(df[features[3]],k)
     return (df)
 
 #Split data into test and train set ; flag_squared : If you want 2nd order regression
-
-def data_split(df,sex_choice,flag_squared):
+def data_split(df,sex_choice,order):
+    X = df.drop(['SR'],axis=1)
     if sex_choice == 'MLE':
-        if flag_squared:
-            X = df[['country_id','AR','MBMI','FPM','Squared AR','Squared MBMI','Squared FPM']]
-        else:
-            X = df[['country_id','AR','MBMI','FPM']]    
-    else:
-        if flag_squared:
-            X = df[['country_id','AR','MBMI','FPM','ABR','Squared AR','Squared MBMI','Squared FPM','Squared ABR']]
-        else:
-            X = df[['country_id','AR','MBMI','FPM','ABR']]
+      X = X.drop(['ABR'], axis=1)
+      if order >1:
+        for k in range(2,order+1):
+          X = X.drop([f'Power {k} of ABR'], axis=1)
 
-    y = df[['country_id','SR']]
-    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2)#,random_state=2)
+    y = df[['SR']]
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
     return (X_train,X_test,y_train,y_test)
 
+#Normalize data
+def normalization(df):
+    names = df.columns
+    if 'country_id' in names:
+      print(True)
+      country_id = df['country_id']
+      print(country_id)
+    if 'continent_id' in names:
+      continent_id = df['continent_id']
+  
+    x = df.values #returns a numpy array
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    df = pd.DataFrame(x_scaled)
+    df = df.set_axis(names, axis=1)#, inplace=False))
+    
+    if 'country_id' in names:
+      print("ICICIICI")
+      print(df)
+      df['country_id'] = country_id 
+      print(df)
+    if 'continent_id' in names:
+      df['continent_id'] = continent_id 
+
+    return df
+
 #Regression function : return the prediction 
-def regression(X_train,X_test,y_train,y_test):
+def regression(X_train,X_test,y_train,y_test,flag_country,flag_continent):
+    if flag_country:
+      X_train = X_train.loc[:, X_train.columns != 'country_id']
+      X_test = X_test.loc[:, X_test.columns != 'country_id']
+      y_train = y_train.loc[:, y_train.columns != 'country_id']
+      y_test = y_test.loc[:, y_test.columns != 'country_id']
+
+    if flag_continent:
+      X_train = X_train.loc[:, X_train.columns != 'continent_id']
+      X_test = X_test.loc[:, X_test.columns != 'continent_id']
+      y_train = y_train.loc[:, y_train.columns != 'continent_id']
+      y_test = y_test.loc[:, y_test.columns != 'continent_id']
+
+    
     LR= LinearRegression()
     LR.fit(X_train,y_train)
     y_predict = LR.predict(X_test)
+    y_test = y_test.to_numpy()
+
 
     score=r2_score(y_test,y_predict)
+    print("Score of regression")
     print('R2 =',score)
     print('MSE =',mean_squared_error(y_test,y_predict))
     print('RMSE =',np.sqrt(mean_squared_error(y_test,y_predict)))
 
-    return (y_predict)
+    error = np.abs(y_test-y_predict)
+
+    return (y_predict,y_test,error)
 
 #Return the list of countries in y_test
 def countries(y_test,id):
     y_test = y_test.to_numpy()
     list_country = []
-    for c in y_test[:,0]:
+    #for c in y_test[:,0]:
+    for c in y_test:
         list_country.append(id[int(c)][0])
 
     return(list_country)
 
 if __name__=='__main__':
-    df = merge_data_sets()
-
+    #df = merge_data_sets()
+    df = read_csv("data/data_set.csv")
     df_regression, countries_id  = country_to_id(df)
+    df_regression, continents_id  = continent_to_id(df)
 
-    data_name = ['country_id',"Period","Dim1ValueCode",'AR','MBMI','FPM','ABR','SR']
-    df_regression = df_regression[data_name]
+    print(df_regression)
+    print('Suicide rate parameters')
+    print('Mean =',df_regression['SR'].mean())
+    print('Std =',df_regression['SR'].std())
 
-    sex_choice = 'BTSX' #sex_choice = 'MLE','FMLE','BTSX'
+    ############ FLAG SELECTION ##############
+    sex_choice = 'MLE' #sex_choice = 'MLE','FMLE','BTSX'
     flag_year = True #Want a specific year, by default is 2014
-    flag_squared = False #Add squared data to dataframe
+    flag_squared = True #Add squared data to dataframe
+    flag_country = True # If you don't want the id of the country in the regression 
+    flag_continent = True # If you don't want the id of the continent in the regression 
+    order = 3 #Order of the polynomial regression 
+
+    ############# DATA_TO_KEEP_FOR_REGRESSION#############
+    data_name = ['country_id','continent_id',"Period","Dim1ValueCode",'AR','MBMI','FPM','ABR','SR']
+    df_regression = df_regression[data_name]
     df_regression = choose_year_sex(df_regression,sex_choice,flag_year)
-
-    if flag_squared:
-        df_regression = squared_data(df_regression)
-
-    X_train,X_test,y_train,y_test = data_split(df_regression,sex_choice,flag_squared)
-    y_predict = regression(X_train,X_test,y_train,y_test)
-    list_countries = countries(y_test,countries_id)
+    df_regression = order_data(df_regression,order)
+    X_train,X_test,y_train,y_test = data_split(df_regression,sex_choice,order)
+    list_countries = countries(X_test['country_id'],countries_id)
     print(list_countries)
+    X_train,X_test = normalization(X_train),normalization(X_test)
+    #y_train,y_test = normalization(y_train),normalization(y_test)
+    #print(X_train)
+    
+    y_predict,y_test,error = regression(X_train,X_test,y_train,y_test,flag_country,flag_continent)
 
-    plt.scatter(list_countries,y_test.to_numpy()[:,1]-y_predict[:,1],color='b')
-    plt.title('Prediction error of the suicide rate per 100,000 inhabitants in absolute value')
+    plt.plot(list_countries,error,color='b')
+    plt.title('Prediction error of the suicide rate per 100,000 inhabitants')
     plt.xticks(rotation=60)
     plt.tick_params(axis='both', which='major', labelsize=8)
-
+    plt.xlabel('Countries')
+    plt.ylabel('Error')
     plt.show()
+    #plt.show(block=False)
+    #plt.pause(15)
+    
+    #plt.close() 
